977dd452911432dd02ac5aa63172437e50f3a003
Your Name <you@example.com>
2021-11-04 17:53:18 +1300

    aws academy updates

--- DeployAWSacademy.sh
+++ DeployAWSacademy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 aws cloudformation create-stack\
  --disable-rollback --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND\
- --stack-name $1\
+ --stack-name $1 --region us-east-1\
  --template-body file://asg-template.yaml --parameters file://.exclDevparams.json
  
