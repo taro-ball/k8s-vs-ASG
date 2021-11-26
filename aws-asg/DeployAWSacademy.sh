c2f2f08d82aaf1844bc98ec8a743e4718a7ef53f
Your Name <you@example.com>
2021-11-26 18:02:44 +1300

    EKS permissions, noIAM asg

--- aws-asg/DeployAWSacademy.sh
+++ aws-asg/DeployAWSacademy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 aws cloudformation create-stack\
- --disable-rollback --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND\
+ --disable-rollback --profile awsacademy --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND\
  --stack-name $1 --region us-east-1\
  --template-body file://asg-template.yaml --parameters file://asg-parameters.json
  
--- aws-eks/EKSdeploy.sh
+++ aws-eks/EKSdeploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-aws cloudformation create-stack\
+aws cloudformation --profile awsacademy create-stack\
  --disable-rollback --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND\
  --stack-name $2\
  --template-body file://eks-controlplane.yaml --parameters file://$1
