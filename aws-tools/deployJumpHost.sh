0490ddd079ba2b2be0cdb43de25127215c1a26d4
Your Name <you@example.com>
2021-12-01 21:06:30 +1300

    generated cluster, fixed jumphost

--- aws-tools/deployJumpHost.sh
+++ aws-tools/deployJumpHost.sh
@@ -5,9 +5,10 @@ vpcID=`aws ec2 describe-vpcs \
     --query 'Vpcs[*].VpcId' \
     --output text`
 
-sbntID=`aws ec2 describe-subnets --output text --query 'Subnets[5].SubnetId'`
+sbntID=`aws ec2 describe-subnets --filters "Name=vpc-id,Values=${vpcID}"  --output text --query 'Subnets[1].SubnetId'`
 
 aws cloudformation create-stack\
  --disable-rollback --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND\
  --stack-name jumphost-$1 --template-body file://jumphost.yaml\
  --parameters ParameterKey=myVPC,ParameterValue=${vpcID} ParameterKey=mySubnet1,ParameterValue=${sbntID}
+
