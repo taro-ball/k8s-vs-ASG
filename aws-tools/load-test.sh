0daab88cb00be275851b2b523002ee74dc6f6d6f
Your Name <you@example.com>
2021-12-25 09:36:39 +1300

    create load-testing script, PS1

--- aws-tools/load-test.sh
+++ aws-tools/load-test.sh
@@ -7,6 +7,6 @@ set -x
 # -qps -1: max queries per second
 # -r 0.01: Resolution of the histogram lowest buckets in seconds
 
-aws elbv2 describe-load-balancers --region us-east-1
+aws elbv2 describe-load-balancers --region us-east-1 --query 'LoadBalancers[*].DNSName' --output text
 
-fortio load -a -c 150 -n 10000000 -qps -1 -r 0.01 http://internal-asg-2-alb-510343582.us-east-1.elb.amazonaws.com:88/test.html
+fortio load -a -c 150 -n 2000000 -qps -1 -r 0.01 http://internal-asg-2-alb-510343582.us-east-1.elb.amazonaws.com:88/test.html
