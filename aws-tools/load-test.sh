5c4344faff12040585365e8e34aee179fa3a5ea1
Your Name <you@example.com>
2022-01-07 19:34:35 +1300

    get data scripts, reverse timestamps

--- aws-tools/load-test.sh
+++ aws-tools/load-test.sh
@@ -7,7 +7,8 @@ set -x
 # -qps -1: max queries per second
 # -r 0.01: Resolution of the histogram lowest buckets in seconds
 
-aws elbv2 describe-load-balancers --region us-east-1 --query 'LoadBalancers[*].DNSName' --output text
+alb=`aws elbv2 describe-load-balancers --region us-east-1 --query 'LoadBalancers[*].DNSName' --output text | grep asg`
+echo $alb
 
-fortio load -a -c 50 -n 2000000 -qps -1 -r 0.01 http://localhost:88/test.html
-fortio load -a -c 120 -t 600s -qps -1 -r 0.01 http://localhost:88/test.html
+fortio load -a -c 50 -n 2000000 -qps -1 -r 0.01 http://$alb:88/test.html
+fortio load -a -c 120 -t 600s -qps -1 -r 0.01 http://$alb:88/test.html
--- data/cli/get-data.sh
+++ data/cli/get-data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-aws cloudwatch get-metric-data --cli-input-json file://requestALB.json --region us-east-1
+aws cloudwatch get-metric-data --cli-input-json file://alb.json --region us-east-1 > alb_data.json
+aws cloudwatch get-metric-data --cli-input-json file://asg.json --region us-east-1 > asg_data.json
