c348d1282ae221f8a7bc92dbdfd673af83a1db5f
Your Name <you@example.com>
2022-01-07 22:25:33 +1300

    update asg size,  load test & data scripts

--- aws-tools/load-test.sh
+++ aws-tools/load-test.sh
@@ -8,7 +8,6 @@ set -x
 # -r 0.01: Resolution of the histogram lowest buckets in seconds
 
 alb=`aws elbv2 describe-load-balancers --region us-east-1 --query 'LoadBalancers[*].DNSName' --output text | grep asg`
-echo $alb
-
+echo $alb;cd;pwd; curl $alb:88/test.html
 fortio load -a -c 50 -n 2000000 -qps -1 -r 0.01 http://$alb:88/test.html
-fortio load -a -c 120 -t 600s -qps -1 -r 0.01 http://$alb:88/test.html
+fortio load -a -c 80 -t 600s -qps -1 -r 0.01 http://$alb:88/test.html
--- data/cli/get-data.sh
+++ data/cli/get-data.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 aws cloudwatch get-metric-data --cli-input-json file://alb.json --region us-east-1 > alb_data.json
 aws cloudwatch get-metric-data --cli-input-json file://asg.json --region us-east-1 > asg_data.json
+
+# get results
+sudo zip -r /var/www/html/foo.zip .
+
+# then you can
+http://100.26.180.59/foo.zip
