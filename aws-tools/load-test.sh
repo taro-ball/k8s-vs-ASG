48f30444fb217c8e2a20e9ffd0f15fdafe2b8f8e
Your Name <you@example.com>
2022-01-27 13:13:30 +1300

    load test labels, classic elb query, scp from jh

--- aws-tools/load-test.sh
+++ aws-tools/load-test.sh
@@ -9,13 +9,16 @@ set -x
 
 alb=`aws elbv2 describe-load-balancers --region us-east-1 --query 'LoadBalancers[*].DNSName' --output text | grep asg`
 echo $alb;cd;pwd; curl $alb:88/test.html
-fortio load -a -c 50 -n 2000000 -qps -1 -r 0.01 http://$alb:88/test.html
+fortio load -a -c 50 -n 2000000 -qps -1 -r 0.01 -labels "hpa24 etc" http://$alb:88/test.html
 fortio load -a -c 80 -t 600s -qps -1 -r 0.01 http://$alb:88/test.html
 
 # quick check
-egrep -rni QPS\|Threads|Count *
+egrep -rni QPS\|Threads\|Count *
 
 # proper check
 jq '{"URL": .Labels,StartTime,NumThreads,ActualQPS,\
 "DurationSeconds": (.ActualDuration/1000000000),"TotalRequests": .DurationHistogram["Count"], \
 "Percentiles":.DurationHistogram["Percentiles"][2]}' 2*
+
+
+# jq '{"URL": .Labels,StartTime,NumThreads,ActualQPS,"DurationSeconds": (.ActualDuration/1000000000),"TotalRequests": .DurationHistogram["Count"],"Percentiles":.DurationHistogram["Percentiles"][2]}' 2*
--- data/cli/copy-to-jh.sh
+++ data/cli/copy-to-jh.sh
@@ -2,3 +2,10 @@
 ip=`aws ec2 describe-instances --filters Name=tag-value,Values=jumphost-1 --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text`
 echo copying to $ip
 scp -o StrictHostKeyChecking=no -i ../../.exclDEV-Key.pem -r $PWD/ ec2-user@${ip}:/tmp
+
+# and back
+ip=`aws ec2 describe-instances --filters Name=tag-value,Values=jumphost-1 --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text`
+mydate=$(date +"%Y-%m-%d-%H-%M")
+echo copying from $ip to $mydate
+mkdir ~/run${mydate}
+scp -o StrictHostKeyChecking=no -i ../../.exclDEV-Key.pem -r ec2-user@${ip}:/tmp/cli ~/run${mydate}
