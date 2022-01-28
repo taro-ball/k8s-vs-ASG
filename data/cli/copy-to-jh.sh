2498b6dabe1033886ef6e7aed0835a56eb4520d8
Your Name <you@example.com>
2022-01-28 13:39:54 +1300

    cluster sizing, ssh on jh automation

--- data/cli/copy-to-jh.sh
+++ data/cli/copy-to-jh.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 ip=`aws ec2 describe-instances --filters Name=tag-value,Values=jumphost-1 --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text`
 echo copying to $ip
-scp -o StrictHostKeyChecking=no -i ../../.exclDEV-Key.pem -r $PWD/ ec2-user@${ip}:/tmp
+# remember to `cd aws-thesis/data/cli`
+scp -o StrictHostKeyChecking=no -r $PWD/ ec2-user@${ip}:/tmp
+# if no ssh config then set key explicitly: -i ../../.exclDEV-Key.pem
+#then
+# on jh
+sudo chmod -R a+rw /tmp/
+sudo cp -r /home/ssm-user/2* /tmp/cli
 
 # and back
 ip=`aws ec2 describe-instances --filters Name=tag-value,Values=jumphost-1 --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text`
@@ -9,3 +15,4 @@ mydate=$(date +"%Y-%m-%d-%H-%M")
 echo copying from $ip to $mydate
 mkdir ~/run${mydate}
 scp -o StrictHostKeyChecking=no -i ../../.exclDEV-Key.pem -r ec2-user@${ip}:/tmp/cli ~/run${mydate}
+
