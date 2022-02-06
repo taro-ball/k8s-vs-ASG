7ec3a08b45d19c23b324149b1a7970c990b2e51e
Your Name <you@example.com>
2022-02-07 10:14:31 +1300

    ASG health 5s, 45 CPU, jh full ec2, scaling 180s

--- aws-tools/node-asg-load.sh
+++ aws-tools/node-asg-load.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
+echo start warmup: $(date) >> dates.txt
+set -x
+
 lb=`aws elb describe-load-balancers --region us-east-1 --query 'LoadBalancerDescriptions[*].DNSName' --output text | sed 's/\s\+/\n/g' | grep asg`
-echo $lb;cd;pwd; curl $lb:3000
+echo $lb;cd;pwd; curl $lb:3000; echo
 
 # scale to max
 myasg=`aws autoscaling describe-auto-scaling-groups --region us-east-1 --query 'AutoScalingGroups[*].AutoScalingGroupName' --output text| sed 's/\s\+/\n/g' | grep asg`
@@ -13,16 +16,17 @@ for((i=15;i<=25;i+=1)); do fortio load -a -c $i -t 30s -qps -1 -r 0.01 -labels "
 # performance
 for((i=1;i<=3;i+=1)); do echo sleep 60; sleep 60; fortio load -a -c 20 -t 300s -qps -1 -r 0.01 -labels "performance-${i}" http://$lb:3000?n=9999; done
 
+echo start scaling: $(date) >> dates.txt
 # scaling
 for((i=1;i<=3;i+=1))
 do
 	# scale to min
     echo scaling to 1;
     aws autoscaling update-auto-scaling-group --region us-east-1 --auto-scaling-group-name $myasg --desired-capacity 1;
-    echo sleep 120;sleep 120;
+    echo sleep 120;sleep 180;
     fortio load -a -c 20 -t 780s -qps -1 -r 0.01 -labels "scaling-${i}" http://$lb:3000?n=9999
 done
-
+echo end: $(date) >> dates.txt
 
 
 
--- data/cli/copy-to-jh.sh
+++ data/cli/copy-to-jh.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ip=`aws ec2 describe-instances --filters Name=tag-value,Values=jumphost-1 --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text`
 echo copying to $ip
-# remember to `cd aws-thesis/data/cli`
+# remember to `cd data/cli`
 scp -o StrictHostKeyChecking=no -r $PWD/ ec2-user@${ip}:/tmp
 # if no ssh config then set key explicitly: -i ../../.exclDEV-Key.pem
 # then
 # on jh (eg from vsode terminal)
 sudo chmod -R a+rw /tmp/
-sudo cp -r /home/ssm-user/2* /tmp/cli
+sudo cp -r /home/ssm-user/* /tmp/cli
 
 # and back
 ip=`aws ec2 describe-instances --filters Name=tag-value,Values=jumphost-1 --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text`
