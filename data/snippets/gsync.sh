ffc09b76f9350d7d866ad156e48942d0363f54de
Your Name <you@example.com>
2022-02-14 20:19:41 +1300

    improve gsync

--- data/snippets/gsync.sh
+++ data/snippets/gsync.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-# copy from gcloud permanent 
-t=-0.4
-last_backups=$(ssh robyn@gcloud "find /home/parasail/* -ctime $t -type d")
-echo $last_backups
-find . -ctime $t -type d
-sleep 999
+# time equals t*24h (should be negative)
+# only works for directories without subfolders
+t=$1
+if  [ -z "$1" ]; then
+echo 'no t specified, setting t to -1 (last 24 hours)'
+t=-1
+fi
+
+last_backups=$(ssh robyn@gcloud "find /home/parasail/* -maxdepth 0 -ctime $t -type d")
+echo remote: $last_backups === local: | xargs -n1
+find ./*  -maxdepth 0 -ctime $t -type d 
+read -p "Press enter to start"
 for folder in $(echo $last_backups); do
     f=${folder##*/}
     if [ ! -d "./$f" ]
