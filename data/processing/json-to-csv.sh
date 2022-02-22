17d7c4998c612b9e3def30de84c5d270d508ae1e
Your Name <you@example.com>
2022-02-23 11:33:41 +1300

    update csv conversion

--- data/processing/json-to-csv.sh
+++ data/processing/json-to-csv.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-jq -r '.MetricDataResults[] | ([.Timestamps, .Values] | transpose[]) + [.Id] | @csv'
+jq -r '.MetricDataResults[] | ([.Timestamps, .Values] | transpose[]) + [.Id] | @csv' asg_data.json > asg_data.csv
+jq -r '.MetricDataResults[] | ([.Timestamps, .Values] | transpose[]) + [.Id] | @csv' alb_data.json > alb_data.csv
