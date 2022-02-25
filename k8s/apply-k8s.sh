21a35f70c243f9cee05998101b7c4a1f496ef81d
Your Name <you@example.com>
2022-02-25 16:25:11 +1300

    pin metrics server, default taro autoscale change

--- k8s/apply-k8s.sh
+++ k8s/apply-k8s.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
 
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.6.1/components.yaml
 kubectl apply -f cluster-autoscaler-autodiscover.yaml
 
 kubectl apply -f taro.yaml
 # kubectl apply -f nginx.yaml
 # kubectl apply -f apache.yaml
 
-kubectl autoscale deployment taro-deployment --cpu-percent=50 --min=1 --max=8
+kubectl autoscale deployment taro-deployment --cpu-percent=50 --min=1 --max=6
 # kubectl autoscale deployment nginx-deployment --cpu-percent=70 --min=1 --max=10
 # kubectl autoscale deployment apache-deployment --cpu-percent=50 --min=1 --max=8
 # kubectl delete horizontalpodautoscaler.autoscaling/apache-deployment
