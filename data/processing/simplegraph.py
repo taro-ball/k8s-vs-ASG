820cff4674ff0f3f69ee95747e645b83e2ac6121
Your Name <you@example.com>
2022-03-09 18:19:44 +1300

    graph: add proper path handling, better plot

--- data/processing/simplegraph.py
+++ data/processing/simplegraph.py
@@ -1,14 +1,29 @@
-import sys
 import pandas as pd
+import numpy as np
+import argparse
+import pathlib
 
-arg = sys.argv[1]
-print(arg)
+parser = argparse.ArgumentParser()
+parser.add_argument("file_path", type=pathlib.Path)
 
-file='C:/Users/pa/OneDrive/7.UniCode/0thesis-code/myjupy/1stTry/asg_data_cpu_normal.csv'
+p = parser.parse_args()
+
+if p.file_path.exists():
+  #and type(p.file_path)
+  print("processing ", p.file_path)
+else:
+  print("File does not exist. Exit.")
+  quit()
+
+
+file=p.file_path
+#fileName=file.name
+#dir=file.parent
+out_file=file.with_suffix('.png')
 
 df = pd.read_csv(file, parse_dates=['date'], index_col="date")
 #df.plot()
 
-plot = df.plot()
+plot = df.plot(kind='line',grid=1,figsize=(7,3.5),yticks=(np.arange(0, 100, 5)))
 fig = plot.get_figure()
-fig.savefig("output.png")
+fig.savefig(out_file)
