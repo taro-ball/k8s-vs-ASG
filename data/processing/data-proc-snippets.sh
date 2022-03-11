16349c1090c1cbaf57fd1d2bc7a3eba61b013220
Your Name <you@example.com>
2022-03-12 11:03:30 +1300

    snippet update

--- data/processing/data-proc-snippets.sh
+++ data/processing/data-proc-snippets.sh
@@ -7,4 +7,5 @@
 find ./2022.03* -maxdepth 0 -type d -exec ./csv_gen.sh {} \;
 
 # 2. plot csv data
+find ./2022.03.11*/csv/*.csv -exec python ../k8s-aws-thesis/data/processing/simplegraph.py {} \;
 find ./2022.03*/csv/*.csv -exec python /c/full/path/simplegraph.py --overwrite {} \;
