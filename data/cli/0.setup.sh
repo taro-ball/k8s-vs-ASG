af09564eb9cd1ccbde5259114c22bc35a94bca03
Your Name <you@example.com>
2022-03-17 15:53:29 +1300

    asg riwai deployment

--- data/cli/0.setup.sh
+++ data/cli/0.setup.sh
@@ -70,4 +70,17 @@ max_nodes=3
 fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
 fi
 
+if [ "$test" == "asg_riwai_3" ]; then
+warmup_url='3000?n=20000'
+testing_url='3000?n=20000'
+cpu_perc=35
+warmup_min_threads=15
+warmup_max_threads=25
+warmup_cycle_sec=90
+scaling_minutes=14
+performance_sec=300
+max_capacity=3
+fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
+fi
+
 set +x
