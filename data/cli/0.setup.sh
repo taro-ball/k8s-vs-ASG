9d2d6796519436ddf8fca4075b65cd90f839618c
Your Name <you@example.com>
2022-03-26 17:51:45 +1300

    apache 2core test

--- data/cli/0.setup.sh
+++ data/cli/0.setup.sh
@@ -27,6 +27,21 @@ max_capacity=3
 fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
 fi
 
+if [ "$test" == "k8s_apache2_3" ]; then
+warmup_url='80/'
+testing_url='80/'
+hpa_perc=70
+warmup_min_threads=60
+warmup_max_threads=70
+warmup_cycle_sec=130
+scaling_minutes=14
+performance_sec=300
+cluster_name="C888"
+max_pods=3
+max_nodes=3
+fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
+fi
+
 if [ "$test" == "k8s_taewa_3" ]; then
 warmup_url='3000/?n=20000'
 testing_url='3000/?n=20000'
@@ -59,8 +74,8 @@ if [ "$test" == "k8s_taewa_3lite" ]; then
 warmup_url='3000/?n=1'
 testing_url='3000/?n=1'
 hpa_perc=70
-warmup_min_threads=25
-warmup_max_threads=35
+warmup_min_threads=50
+warmup_max_threads=60
 warmup_cycle_sec=90
 scaling_minutes=14
 performance_sec=300
@@ -74,8 +89,8 @@ if [ "$test" == "asg_taewa_3lite" ]; then
 warmup_url='3000/?n=1'
 testing_url='3000/?n=1'
 cpu_perc=35
-warmup_min_threads=25
-warmup_max_threads=35
+warmup_min_threads=50
+warmup_max_threads=60
 warmup_cycle_sec=90
 scaling_minutes=14
 performance_sec=300
