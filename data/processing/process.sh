f8314e6296a932682b07ec3aff350e398eb91147
Your Name <you@example.com>
2022-05-04 17:51:35 +1200

    update explorer heading, add to processing

--- data/processing/process.sh
+++ data/processing/process.sh
@@ -6,11 +6,16 @@ find ./$1* -maxdepth 0 -type d -exec ./csv_gen.sh {} \;
 # 1.2 generate cobined fortio json
 find ./$1* -maxdepth 0 -type d -exec ./fortio_prc.sh {} \;
 
+
 # 2.1 plot fortio data
 fg="python ../k8s-aws-thesis/data/processing/foldergraph.py --overwrite"
 find ./$1*/csv -maxdepth 0 -type d -exec $fg {} \; -exec $fg --metric cpuUtilization {} \; \
-  -exec $fg --metric groupInServiceCapacity {} \; -exec $fg --metric requestCount --threads {} \;
-  
-# 2.2 plot csv data
-find ./$1*/csv/*.csv -exec python ../k8s-aws-thesis/data/processing/simplegraph.py {} \;
+  -exec $fg --metric groupInServiceCapacity {} \; -exec $fg --metric requestCount --threads {} \; \
+  -exec $fg --metric backendConnectionErrors {} \;
 
+# 2.2 plot csv data
+find ./$1*/csv/*.csv -exec python ../k8s-aws-thesis/data/processing/simplegraph.py --overwrite {} \;
+
+# 3.0 re-generate results explorer
+cd ../k8s-aws-thesis/data/processing
+ipython -c "%run resultsExplorer.ipynb"
