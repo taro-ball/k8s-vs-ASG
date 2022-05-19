2fadf8f14a8213620c91ebbe7821600b046ccadc
Your Name <you@example.com>
2022-05-19 21:31:06 +1200

    add taewa2 app and test

--- data/cli/0.setup.sh
+++ data/cli/0.setup.sh
@@ -57,6 +57,21 @@ max_nodes=3
 fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
 fi
 
+if [ "$test" == "k8s_taewa2_3" ]; then
+warmup_url='3000/?n=20000'
+testing_url='3000/?n=20000'
+hpa_perc=35
+warmup_min_threads=15
+warmup_max_threads=25
+warmup_cycle_sec=90
+scaling_minutes=14
+performance_sec=300
+cluster_name="C888"
+max_pods=6
+max_nodes=3
+fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
+fi
+
 if [ "$test" == "asg_taewa_3" ]; then
 warmup_url='3000/?n=20000'
 testing_url='3000/?n=20000'
