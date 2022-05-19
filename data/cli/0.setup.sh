3f75a79043882d9028e97872f7c0e6160b21ab50
Your Name <you@example.com>
2022-05-19 19:00:15 +1200

    add asg_raupi_3_70 test with fixed scaling

--- data/cli/0.setup.sh
+++ data/cli/0.setup.sh
@@ -196,6 +196,19 @@ max_capacity=3
 fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
 fi
 
+if [ "$test" == "asg_raupi_3_70" ]; then
+warmup_url='3000/?n=200'
+testing_url='3000/?n=200'
+cpu_perc=70
+warmup_min_threads=15
+warmup_max_threads=25
+warmup_cycle_sec=90
+scaling_minutes=14
+performance_sec=300
+max_capacity=3
+fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
+fi
+
 if [ "$test" == "k8s_raupi_3" ]; then
 warmup_url='3000/?n=200'
 testing_url='3000/?n=200'
