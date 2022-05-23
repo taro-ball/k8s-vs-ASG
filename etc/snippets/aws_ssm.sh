a0bc82d995757f158209005cb0781406c195e9d5
Your Name <you@example.com>
2022-05-23 17:43:15 +1200

    better arrangement

--- etc/snippets/aws_ssm.sh
+++ etc/snippets/aws_ssm.sh
@@ -1,2 +1,3 @@
+# https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html#install-plugin-windows
 aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId,ImageId,Tags[*]]"
 aws ssm start-session --target i-0myInstanceId
