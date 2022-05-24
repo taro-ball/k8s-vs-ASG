61fbc3912dda5486101c701c140fdaa0c753b14b
Your Name <you@example.com>
2022-05-25 09:38:00 +1200

    more tests

--- etc/snippets/aws_ssm.sh
+++ etc/snippets/aws_ssm.sh
@@ -1,3 +1,3 @@
 # https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html#install-plugin-windows
-aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId,ImageId,Tags[*]]"
+aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId,ImageId,Tags[*]]" | grep -b 10 jumphost
 aws ssm start-session --target i-0myInstanceId
