d071cb0e02afc225d095833713ae71649e2f7048
Your Name <you@example.com>
2022-05-28 12:56:12 +1200

    add raupi2 test

--- data/cli/0.setup.sh
+++ data/cli/0.setup.sh
@@ -255,4 +255,19 @@ max_nodes=3
 fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
 fi
 
+if [ "$test" == "k8s_raupi2_3" ]; then
+warmup_url='3000/?n=200'
+testing_url='3000/?n=200'
+hpa_perc=70
+warmup_min_threads=15
+warmup_max_threads=25
+warmup_cycle_sec=90
+scaling_minutes=14
+performance_sec=300
+cluster_name="C888"
+max_pods=3
+max_nodes=3
+fortio_options="-a -qps -1 -r 0.01 -loglevel Error -allow-initial-errors"
+fi
+
 set +x
