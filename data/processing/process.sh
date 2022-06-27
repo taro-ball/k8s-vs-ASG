62c6c77e18928ecd3e9fed58bab4990d8a077b08
Your Name <you@example.com>
2022-06-27 12:57:22 +1200

    folder name consistent with repo name

--- data/processing/process.sh
+++ data/processing/process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# please copy this file to the results directory
 # 1.1 generate csv from AWS json
 find ./$1* -maxdepth 0 -type d -exec ./csv_gen.sh {} \;
 
@@ -8,15 +8,14 @@ find ./$1* -maxdepth 0 -type d -exec ./fortio_prc.sh {} \;
 
 
 # 2.1 plot fortio data
-fg="python ../k8s-aws-thesis/data/processing/foldergraph.py --overwrite"
+fg="python ../thesis-infra/data/processing/foldergraph.py --overwrite"
 find ./$1*/csv -maxdepth 0 -type d -exec $fg {} \; -exec $fg --metric cpuUtilization {} \; \
   -exec $fg --metric groupInServiceCapacity {} \; -exec $fg --metric requestCount --threads {} \; \
   -exec $fg --metric backendConnectionErrors {} \;
 
 # 2.2 plot csv data
-find ./$1*/csv/*.csv -exec python ../k8s-aws-thesis/data/processing/simplegraph.py --overwrite {} \;
+find ./$1*/csv/*.csv -exec python ../thesis-infra/data/processing/simplegraph.py --overwrite {} \;
 
 # 3.0 re-generate results explorer
-rm o_summary.csv
-cd ../k8s-aws-thesis/data/processing
+cd ../thesis-infra/data/processing;ls
 ipython -c "%run resultsExplorer.ipynb"
